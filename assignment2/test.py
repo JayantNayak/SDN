1b9ac7ea432f5a0d27624dd82d5848f4598872a7
JayantNayak <jayant.kumar.nayak92@gmail.com>
2014-07-08 03:16:20 +0530

    These repository consists of the various assignments I did in SDN course in cousera.

---
+++ assignment2/test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+from mininet.net import Mininet
+from mininet.util import dumpNodeConnections
+from mininet.log import setLogLevel
+from mininet.link import TCLink
+from CustomTopo import *
+
+
+if __name__=='__main__':
+    setLogLevel('info')
+    linkopts1 = {'bw':50, 'delay':'5ms'}
+    linkopts2 = {'bw':30, 'delay':'10ms'}
+    linkopts3 = {'bw':10, 'delay':'15ms'}
+
+    topo = CustomTopo(linkopts1, linkopts2, linkopts3, fanout=2)
+    net = Mininet(topo=topo, link=TCLink)
+    net.start()
+    net.pingAll()
+    dumpNodeConnections(net.hosts)
+    net.stop()
